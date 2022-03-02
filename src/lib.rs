c50c10eef8c1dd393b0cf6e2a177deb93ebd7b7b
Jerry1098 <jeremias1098@gmail.com>
2022-03-02 12:14:54 +0100

    add timer interrupt

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,8 @@ pub extern "C" fn _start() -> ! {
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 pub trait Testable {
