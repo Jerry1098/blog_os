b81f28de65f8537902438f731603f34d9ed9353e
Jerry1098 <jeremias1098@gmail.com>
2022-03-02 12:27:14 +0100

    use hlt_loop

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ pub mod vga_buffer;
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    loop {}
+    hlt_loop();
 }
 
 pub fn init() {
@@ -60,7 +60,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop();
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
