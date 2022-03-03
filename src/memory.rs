fe48e137c6cb233a258ba29476c0bbd0eccd32ea
Jerry1098 <jeremias1098@gmail.com>
2022-03-03 21:13:26 +0100

    add PageTable from x86_64 crate

--- src/memory.rs
+++ src/memory.rs
@@ -1,12 +1,19 @@
-use x86_64::{structures::paging::PageTable, VirtAddr};
+use x86_64::{PhysAddr, structures::paging::PageTable, VirtAddr};
+use x86_64::structures::paging::OffsetPageTable;
 
-/// Returns a mutable reference to the active level 4 table.
+/// Initialize a new OffsetPageTable.
 ///
 /// This function is unsafe because the caller must guarantee that the
 /// complete physical memory is mapped to virtual memory at the passed
 /// `physical_memory_offset`. Also, this function must be only called once
 /// to avoid aliasing `&mut` references (which is undefined behavior).
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     use x86_64::registers::control::Cr3;
 
     let (level_4_table_frame, _) = Cr3::read();
@@ -16,4 +23,48 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
     let page_table_pointer: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_pointer
+}
+
+
+/// Translates the given virtual address to the mapped physical address, or
+/// `None` if the address is not mapped.
+///
+/// This function is unsafe because the caller must guarantee that the
+/// complete physical memory is mapped to virtual memory at the passed
+/// `physical_memory_offset`.
+#[allow(dead_code)]
+unsafe fn translate_addr(addr: VirtAddr, physical_memory_offet: VirtAddr) -> Option<PhysAddr>{
+    translate_addr_inner(addr, physical_memory_offet)
+}
+
+/// Private function that is called by `translate_addr`.
+///
+/// This function is safe to limit the scope of `unsafe` because Rust treats
+/// the whole body of unsafe functions as an unsafe block. This function must
+/// only be reachable through `unsafe fn` from outside of this module.
+fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    use x86_64::structures::paging::page_table::FrameError;
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let table_indexes = [ addr.p4_index(), addr.p3_index(), addr.p2_index(), addr.p1_index()];
+    let mut frame = level_4_table_frame;
+
+    for &index in &table_indexes {
+        // convert the frame into a page table reference
+        let virt = physical_memory_offset + frame.start_address().as_u64();
+        let table_ptr: *const PageTable = virt.as_ptr();
+        let table = unsafe {&*table_ptr};
+
+        // read page table entry and update frame
+        let entry = &table[index];
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
+        }
+    }
+
+    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
