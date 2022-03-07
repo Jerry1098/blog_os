421ce7c8a09ecafdb4c461ff8fbf6443e6bcb768
Jeremias Friedrich <jeremias1098@gmail.com>
2022-03-07 13:43:52 +0100

    impl linked_list allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -5,8 +5,10 @@ use x86_64::structures::paging::mapper::MapToError;
 use x86_64::structures::paging::{FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB};
 use x86_64::VirtAddr;
 use crate::allocator::bump::BumpAllocator;
+use crate::allocator::linked_list::LinkedListAllocator;
 
 pub mod bump;
+pub mod linked_list;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
@@ -51,7 +53,7 @@ unsafe impl GlobalAlloc for Dummy {
 }
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
 
 /// A wrapper around spin::Mutex to permit trait implementations
 pub struct Locked<A> {
